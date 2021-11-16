7ac4ea731b554d6ba4a207a7ef143263c7909dcd
alookatommorow <hessjohnt@gmail.com>
2021-11-16 08:35:31 -0800

    fix pre commit hook

--- client/src/spotify/creds.js
+++ client/src/spotify/creds.js
@@ -1,9 +1,8 @@
 const Credentials = () => {
-
   return {
     ClientId: 'a1516982e6044f1cac493ab1c8492818',
-    ClientSecret: 'a3be153a02124321bdbd647aa70732b4'
-  }
-}
+    ClientSecret: 'a3be153a02124321bdbd647aa70732b4',
+  };
+};
 
 export { Credentials };
